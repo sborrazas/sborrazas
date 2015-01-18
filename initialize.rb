c707b3f6cd98f704a0fc7c05e32c50b390047686
Sebastian Borrazas <seba.borrazas@gmail.com>
2015-01-17 23:35:34 -0200

    Update provisioning/deployment setup

--- initialize.rb
+++ initialize.rb
@@ -2,11 +2,6 @@ def root_path(*args)
   File.join(File.dirname(__FILE__), *args)
 end
 
-require 'rubygems'
-require 'bundler'
-require 'sinatra/base'
+require "sinatra/base"
 
-ENV['BUNDLE_GEMFILE'] = root_path('Gemfile')
-Bundler.setup
-
-require root_path('app/controllers/application_controller')
+require root_path("app/controllers/application_controller")
